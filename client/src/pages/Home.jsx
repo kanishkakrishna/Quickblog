5a7fb7801d249f421fd5e27b45b77fe747132894
Veer Pardeshi <139315910+8767059947@users.noreply.github.com>
2025-07-09 13:53:10 +0530

    Update Home.jsx
    edited Navbar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import BlogList from '../components/BlogList'
 import NewsLetter from '../components/NewsLetter'
